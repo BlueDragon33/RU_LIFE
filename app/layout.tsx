f4f3a167a3b5a653ba4dc5282cd884f095a82397
BlueDragon33 <dinhnam331007@gmail.com>
2026-09-08 15:22:16 +0700

    style: load RU_LIFE content interface styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
+import "./content.css";
 import ServiceWorkerRegister from "@/components/service-worker-register";
 
 export const metadata: Metadata = {
